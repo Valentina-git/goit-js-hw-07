ed29ecd5dae9f33ee12a221f760b00e6ee1c4d36
Valentina <dert12345f@ukr.net>
2020-12-01 18:41:23 +0200

    edit

--- js/task_01.js
+++ js/task_01.js
@@ -64,14 +64,6 @@ items.forEach((listItem) => {
 
 
 
-/**
- * let itemLi = document.querySelectorAll('.item')
-console.log(`В списке ${itemLi.length} категории.`);
-itemLi.forEach((item) => {
-    let h2 = item.querySelector('h2');
-    let li = item.querySelectorAll('li');
-    console.log(`Категория: ${h2.textContent}\nКоличество элементов: ${li.length}`);
-  })
- */
+
 
 
